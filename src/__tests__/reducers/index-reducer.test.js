14f5dc6be79ba80292b21dc1a3faba1e23918801
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-07-07 16:21:25 -0700

    deconstruct function from this.props. Add new tests.

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,5 +1,7 @@
 import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import ticketListReducer from '../../reducers/ticket-list-reducer';
 
 let store = createStore(rootReducer);
 
@@ -9,5 +11,33 @@ describe("rootReducer", () => {
       masterTicketList: {},
       formVisibleOnPage: false
     });
+  });
+
+  test('check that initial state of ticketListReducer matches root reducer', () => {
+    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, {type:null}));
+  });
+
+  test('check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, {type: null}));
+  });
+
+  test('check that initial state of ticketListReducer matches root reducer', () => {
+    const action = {
+      type: "ADD_TICKET",
+      names: "ryan and antonio",
+      location: '4b',
+      issue: "react is hard",
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, action));
+  });
+
+  test('check that initial state of formVisibleReducer matches root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
   });
 });
